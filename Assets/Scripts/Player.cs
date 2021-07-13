282cd194c65c8bb2b3523881967f3ca0077fc682
Periklis Paschaloglou <perkoules@gmail.com>
2021-07-13 14:08:15 +0100

    Start rooms

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 5;
+    [SerializeField] private CharacterController characterController;
+    [SerializeField] private Transform cam;
+
+    public float smoothTurnTime = 0.1f;
+    float smoothTurnVelocity;
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0, vertical).normalized;
+
+        if (direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref smoothTurnVelocity, smoothTurnTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+
+            characterController.Move(moveDir.normalized * speed * Time.deltaTime);
+        }
+    }
+}
